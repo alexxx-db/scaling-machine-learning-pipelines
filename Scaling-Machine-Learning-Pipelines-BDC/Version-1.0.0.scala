a5612c870a62949f8d624ea3f3d2733dd6835291
SireInsectus <SireInsectus>
2021-10-26 16:01:43 +0000

    Removed BDC variant

--- Scaling-Machine-Learning-Pipelines-BDC/Version-1.0.0.scala
+++
@@ -1,8 +0,0 @@
-// Databricks notebook source
-// MAGIC 
-// MAGIC %md # Course: SMLP
-// MAGIC * Version 1.0.0
-// MAGIC * Built 2021-10-26 14:41:41 UTC
-// MAGIC * Git revision: 04251af3e4c2658737de58d7d275f082274d719f
-// MAGIC 
-// MAGIC Copyright © 2021 Databricks, Inc.
